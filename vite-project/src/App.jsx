cba07eba2ad8ffdde8b0b913c4503a19d6ad2115
tn.khanh <truongn.khanh@dai-ichi-life.com.vn>
2024-04-03 15:37:15 +0700

    TNK-004: JSX Loop Inside

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,26 +1,17 @@
 
 const App = () => {
 
-  let marks = 51;
+  const city = ["Dhaka", "USA", "Delhi", "CTG"]
 
   return (
     <div>
-      {( () => {
-
-        if(marks >= 80 && marks < 100){
-          return <h1>A+</h1>
+      <ul>
+        {
+          city.map((item, i) => {
+            return <li key={i.toString()}>{item}</li>
+          })
         }
-        else if (marks > 70 && marks < 80){
-          return <h1>A</h1>
-        }
-        else if (marks > 60 && marks < 70){
-          return <h1>A-</h1>
-        }
-        else {
-          return <h1>F</h1>
-        }
-
-        })()}
+      </ul>
     </div>
   );
 };
