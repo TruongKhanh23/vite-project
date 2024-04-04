9c39f7a0769ca46f7e18dd96a7d5ca39a860558b
tn.khanh <truongn.khanh@dai-ichi-life.com.vn>
2024-04-04 17:09:37 +0700

    TNK-006: JSX Conditional Rendering Using Switch Statement

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,19 +1,17 @@
-const LoginStatusButton = (status) => {
-  if(status){
-    return <button>Logout</button>
-  } else {
-    return <button>Login</button>
-  }
-}
+
 
 const App = () => {
 
-    return (
-      <div>
-        <h1>Login Status</h1>
-        {LoginStatusButton(false)}
-      </div>
-    );  
+    const status = false
+
+    switch(status){
+      case true:
+        return <button>Logout</button>
+      case false:
+        return <button>Login</button>
+      default:
+        return null
+    }
   
 };
 
