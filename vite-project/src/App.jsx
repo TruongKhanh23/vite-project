403b7dcc8a4e23eec9b298224cb9ced891c0f978
tn.khanh <truongn.khanh@dai-ichi-life.com.vn>
2024-04-10 16:45:40 +0700

    TNK-008:  Applying React Router Dom

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,13 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from './pages/AboutPage';
+import ContactPage from "./pages/ContactPage";
+import NotFound from './pages/NotFound';
 
 const App = () => {
 
-  let status = false
-
   return (
     <div>
-      {
-        status ? <button>Logout</button> : <button>Login</button>
-      }
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/about" element={<AboutPage/>}/>
+          <Route path="/contact" element={<ContactPage/>}/>
+          <Route path="*" element={<NotFound/>}/>
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 };
